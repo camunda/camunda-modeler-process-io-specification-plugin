6be171b0a770af5fecc9a0e41f0f42ca80949c1c
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2021-12-15 13:05:06 +0100

    feat: migrate to Camunda Modeler 5

---
+++ client/properties-panel/extensions-helper.js
@@ -0,0 +1,73 @@
+import { isAny } from 'bpmn-js/lib/features/modeling/util/ModelingUtil';
+import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
+
+export function findExtensions(element, types) {
+  const extensionElements = getExtensionElements(element);
+
+  if (!extensionElements) {
+    return [];
+  }
+
+  return extensionElements.get('values').filter((value) => {
+    return isAny(value, [].concat(types));
+  });
+}
+
+export function getExtensionElements(element) {
+  const businessObject = getBusinessObject(element);
+
+  return businessObject.get('extensionElements');
+}
+
+export function getCamundaProperties(element) {
+  const bo = getBusinessObject(element);
+
+  const properties = findExtensions(bo, 'camunda:Properties') || [];
+
+  if (properties.length) {
+    return properties[0];
+  }
+
+  return null;
+}
+
+export function createExtensionElements(element, bpmnFactory) {
+  const bo = getBusinessObject(element);
+
+  return createElement('bpmn:ExtensionElements', { values: [] }, bo, bpmnFactory);
+}
+
+export function createCamundaProperties(extensionElements, bpmnFactory, properties) {
+  return createElement('camunda:Properties', properties, extensionElements, bpmnFactory);
+}
+
+function getIoProperties(element, type) {
+  const properties = getProperties(element);
+
+  return (
+    properties
+      .filter(property => isIoProperty(property))
+      .filter(property => parseIoProperty(property).type === type)
+  );
+}
+
+function getProperties(element) {
+  const propertiesParent = getInputOutput(element);
+
+  return propertiesParent ? propertiesParent.get('values') : [];
+}
+
+function getInputParameters(element) {
+  return getIoProperties(element, 'input');
+}
+
+function getOutputParameters(element) {
+  return getIoProperties(element, 'output');
+}
+
+export function createElement(elementType, properties, parent, factory) {
+  const element = factory.create(elementType, properties);
+  element.$parent = parent;
+
+  return element;
+}
--- client/properties-panel/process-io-extension-provider.js
+++ client/properties-panel/process-io-extension-provider.js
@@ -1,87 +1,13 @@
-import inherits from 'inherits';
-
-import CamundaPropertiesProvider from
-  'bpmn-js-properties-panel/lib/provider/camunda/CamundaPropertiesProvider';
-
 import {
   is,
   getBusinessObject
 } from 'bpmn-js/lib/util/ModelUtil';
 
-import processIoProps from './props/process-io-props';
-import processIoEntryProps from './props/process-io-entry-props';
-
 import {
-  parseIoProperty
-} from './process-io-helper';
+  createInputSpecificationGroup,
+  createOutputSpecificationGroup
+} from './props/process-io-groups';
 
-
-function getIoParameterLabel(param) {
-
-  const { type } = parseIoProperty(param);
-
-  if (type === 'input') {
-    return 'Input Definition';
-  }
-
-  if (type === 'output') {
-    return 'Output Definition';
-  }
-
-  return '';
-};
-
-/**
- * Create our extension group tab with the contained groups.
- */
-function createProcessIoTab(element, injector) {
-
-  var processIoGroup = {
-    id: 'process-io-group',
-    label: 'Parameters',
-    entries: []
-  };
-
-  // create groups showing input and output parameters
-
-  var {
-    getSelectedParameter
-  } = injector.invoke(processIoProps, null, { group: processIoGroup, element: element });
-
-  var processIoEntryGroup = {
-    id: 'process-io-entry-group',
-    entries: [],
-    enabled: function(element, node) {
-      return getSelectedParameter(element, node);
-    },
-    label: function(element, node) {
-      var property = getSelectedParameter(element, node);
-
-      return property && getIoParameterLabel(property) || '';
-    }
-  };
-
-  // create single entry edit group
-
-  injector.invoke(processIoEntryProps, null, {
-    group: processIoEntryGroup,
-    element: element,
-    options: {
-      getSelectedParameter
-    }
-  });
-
-  var processIoTab = {
-    id: 'process-io-tab',
-    label: 'I/O Specification',
-    groups: [
-      processIoGroup,
-      processIoEntryGroup
-    ]
-  };
-
-  return processIoTab;
-}
 
 
 /**
@@ -90,36 +16,38 @@ function createProcessIoTab(element, injector) {
  *
  * @param {didi.Injector} injector
  */
-export default function ProcessIoExtensionProvider(injector) {
+export default class ProcessIoExtensionProvider {
+  constructor(propertiesPanel, injector) {
+    this._injector = injector;
+    propertiesPanel.registerProvider(this);
+  }
 
-  injector.invoke(CamundaPropertiesProvider, this);
+  getGroups(element) {
+    return groups => {
+      if (!is(element, 'bpmn:Process') || is(element, 'bpmn:Participant') && !getProcessRef(element)) {
+        return groups;
+      }
 
+      groups = groups.slice();
 
-  var originalGetTabs = this.getTabs.bind(this);
+      groups.splice(1, 0,
+        createInputSpecificationGroup(element, this._injector),
+        createOutputSpecificationGroup(element, this._injector)
+      );
 
-  this.getTabs = function(element) {
-
-    var tabs = originalGetTabs(element);
-
-    var bo = getBusinessObject(element);
-
-    if (
-      is(bo, 'bpmn:Process') || (
-        is(bo, 'bpmn:Participant') &&
-        bo.processRef
-      )
-    ) {
-      var positionTab = createProcessIoTab(element, injector);
-
-      tabs = tabs.slice(0, 1).concat(positionTab).concat(tabs.slice(1));
+      return groups;
     }
-
-    return tabs;
-  };
+  }
 }
 
-inherits(ProcessIoExtensionProvider, CamundaPropertiesProvider);
-
 ProcessIoExtensionProvider.$inject = [
+  'propertiesPanel',
   'injector'
 ];
+
+
+function getProcessRef(element) {
+  const bo = getBusinessObject(element);
+
+  return bo.processRef;
+}
