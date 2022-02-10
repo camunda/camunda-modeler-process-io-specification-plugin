fa51333e374f921bbae77d8eaa3d24d455747bbc
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2022-02-10 14:33:41 +0100

    fix: remove reference to non-existing file
    Closes #13

--- index.js
+++ index.js
@@ -3,6 +3,5 @@
 module.exports = {
   name: 'BPMN Process Input/Output Definitions',
   menu: './menu/menu.js',
-  script: './client/client-bundle.js',
-  style: './style/style.css'
+  script: './client/client-bundle.js'
 };
